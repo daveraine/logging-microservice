e9e1495cd9f1e55034aa1d39134c300d9c393803
David Raine <daveraine@users.noreply.github.com>
2017-10-12 22:05:40 +0100

    Add MessageLogController

---
+++ src/LoggingMicroservice.Tests/Controllers/MessageLogControllerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace LoggingMicroservice.Tests.Controllers
+{
+    using System.Net;
+    using LoggingMicroservice.Controllers;
+    using LoggingMicroservice.Core;
+    using Microsoft.AspNetCore.Mvc;
+    using Xunit;
+
+    public class MessageLogControllerTests
+    {
+        private MessageLogController controller;
+
+        public MessageLogControllerTests()
+        {
+            controller = new MessageLogController();
+        }
+
+        [Fact]
+        public void Log_InvalidModelState_ReturnsBadRequest()
+        {
+            controller.ModelState.AddModelError("Id", "Invalid");
+
+            var result = controller.Log(new Message()) as ObjectResult;
+
+            Assert.Equal((int)HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Fact]
+        public void Log_ValidModelState_ReturnsOkResult()
+        {
+            var result = controller.Log(new Message()) as StatusCodeResult;
+
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
---
+++ src/LoggingMicroservice/Controllers/MessageLogController.cs
@@ -0,0 +1,21 @@
+﻿namespace LoggingMicroservice.Controllers
+{
+    using LoggingMicroservice.Core;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Produces("application/json")]
+    [Route("api/MessageLog")]
+    public class MessageLogController : Controller
+    {
+        [HttpPost]
+        public IActionResult Log(Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+    }
+}
